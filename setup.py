1e61f06a04e8ef8db1eb4503cf717cef7f6ed211
Daniel Miller <danielm@vs-networks.com>
2012-04-20 11:56:42 -0400

    Fix setup.py again.

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+import os.path
 from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='PyBug',
     version='1.0',
     author='Daniel Miller',
+    author_email='millerdev@gmail.com',
     keywords='debug pdb extension',
     url='http://github.com/millerdev/PyBug',
-    description=open('README.md').read(),
+    description=("PyBug extends Python's interactive debugger (pdb), making "
+        "it simpler to add breakpoints to your code."),
+    long_description=read('README.md'),
     py_modules=['bug'],
 )
